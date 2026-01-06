f7ccf99e2669708bd3573826315a97f99fe677dc
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-06 19:09:34 +0530

    adding a new element at the index 0

--- DSA/arrays.py
+++ DSA/arrays.py
@@ -1,18 +1,30 @@
+# from array import * # from array import all 
+# val = array('i',[1,2,3,4,5,6,7,8,9,10])  # 'i' --> integer (usually 2 bytes)
+
+# for i in range(0,len(val)): # better syntax
+#     print(val[i],end=" ")
+
+# print("\n")
+# for x in val: # enhance for loop to print the array
+#     print(x,end=" , ")
+
+
+
+# from array import * # from array import all 
+# val = array('i',[1,2,3,4,5,6,7,8,9,10])  # 'i' --> integer (usually 2 bytes)
+
+# print("\n")
+# print(val.typecode) # prints i ---> typecode is integer
+
+# val.reverse() # reverse the array
+# for i in range(0,len(val)):
+#     print(val[i],end=" ")
+
 
 from array import * # from array import all 
-
 val = array('i',[1,2,3,4,5,6,7,8,9,10])  # 'i' --> integer (usually 2 bytes)
 
-for i in range(0,len(val)): # better syntax
-    print(val[i],end=" ")
 
-print("\n")
-for x in val: # enhance for loop to print the array
-    print(x,end=" , ")
-
-print("\n")
-print(val.typecode) # prints i ---> typecode is integer
-
-val.reverse() # reverse the array
+val.insert(0,50) # 0 is the index and 50 is the new adding element
 for i in range(0,len(val)):
     print(val[i],end=" ")
