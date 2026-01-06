7b585f43ffcc25cee7d302cf7057c6d74874ff50
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-06 18:38:32 +0530

    printing the typecode

--- DSA/arrays.py
+++ DSA/arrays.py
@@ -1,11 +1,14 @@
 
 from array import * # from array import all 
 
-val = array('i',[1,2,3,4,5,6,7,8,9,10])  # 'i' is signed integer (usually 4 bytes)
+val = array('i',[1,2,3,4,5,6,7,8,9,10])  # 'i' --> integer (usually 2 bytes)
 
-# for i in range(0,6): # prints upto 6
-for i in range(0,len(val)):
+for i in range(0,len(val)): # better syntax
     print(val[i],end=" ")
+
 print("\n")
 for x in val: # enhance for loop to print the array
     print(x,end=" , ")
+
+print("\n")
+print(val.typecode) # prints i ---> typecode is integer
