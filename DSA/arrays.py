639bf37962c7948c7b8b0d7e198e5569fc89f4df
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-06 18:22:04 +0530

    better for loop form and increase length of array elements

--- DSA/arrays.py
+++ DSA/arrays.py
@@ -1,6 +1,11 @@
-import array
 
-val = array.array('i',[10,11,20,22,25,34,54,23])  # 'i' is signed integer (usually 4 bytes)
+from array import * # from array import all 
 
-for i in range(0,8):
+val = array('i',[1,2,3,4,5,6,7,8,9,10])  # 'i' is signed integer (usually 4 bytes)
+
+# for i in range(0,6): # prints upto 6
+for i in range(0,len(val)):
     print(val[i],end=" ")
+print("\n")
+for x in val: # enhance for loop to print the array
+    print(x,end=" , ")
