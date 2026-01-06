6e6d02e5d91e6fc2c881992cde6ff498a11c6faa
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-06 19:20:11 +0530

    i am using the append method

--- DSA/arrays.py
+++ DSA/arrays.py
@@ -21,10 +21,19 @@
 #     print(val[i],end=" ")
 
 
+# from array import * # from array import all 
+# val = array('i',[1,2,3,4,5,6,7,8,9,10])  # 'i' --> integer (usually 2 bytes)
+
+
+# val.insert(0,50) # 0 is the index and 50 is the new adding element
+# for i in range(0,len(val)):
+#     print(val[i],end=" ")
+
 from array import * # from array import all 
 val = array('i',[1,2,3,4,5,6,7,8,9,10])  # 'i' --> integer (usually 2 bytes)
 
 
-val.insert(0,50) # 0 is the index and 50 is the new adding element
+val.append(50) # at the element at the end
+val[2] = 100 # replace the second element with 100
 for i in range(0,len(val)):
     print(val[i],end=" ")
