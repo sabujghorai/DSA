eb81c9146822db2610eb6c3fb730bd9881211b3a
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-07 20:11:01 +0530

    uncommenting all

--- DSA/multidimentional_array.py
+++ DSA/multidimentional_array.py
@@ -1,14 +1,14 @@
-# #zero dimentional array
-# from numpy import *
-# a = array(10)
-# print(a)
-# print("\n")
+#zero dimentional array
+from numpy import *
+a = array(10)
+print(a)
+print("\n")
 
-# # 1-D array
-# from numpy import *
-# a = array([1,2,3,4,5,6,7,8,9,10])
-# print(a)
-# print("\n")
+# 1-D array
+from numpy import *
+a = array([1,2,3,4,5,6,7,8,9,10])
+print(a)
+print("\n")
 
 # 2-D array
 from numpy import *
