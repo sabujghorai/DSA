a2c53e6050da1b57f1e11b8ba6262a15f93a4374
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-07 17:30:08 +0530

    taking both the element and index element from user

--- DSA/array_user_input.py
+++ DSA/array_user_input.py
@@ -10,8 +10,25 @@
 # for x in a:
 #     print(x,end=" ")
 
-from array import *
-arr = array('i',[1,2,34,23,45,43,67,32])
+# from array import *
+# arr = array('i',[1,2,34,23,45,43,67,32])
 
-a = arr.index(43)
-print(a)
+# a = arr.index(43)
+# print(a)
+
+from array import *
+arr = array('i',[])
+
+b = int(input("Enter how many numbers :"))
+
+for i in range(0,b):
+    arr.append(int(input("Enter next number :")))
+
+for x in arr:
+    print(x,end=" ")
+print("\n")
+
+
+m = (int(input("Enter any one number of your given element :")))
+
+print("index of your number is ",arr.index(m))
