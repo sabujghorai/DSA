b34becfc92d200a7bd3f02ee39bca487793d7827
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-07 20:12:21 +0530

    uncommenting all

--- DSA/array_user_input.py
+++ DSA/array_user_input.py
@@ -1,20 +1,20 @@
-# from array import *
+from array import *
 
-# a = array('i',[])
+a = array('i',[])
 
-# b = (int(input("Enter how many  numbers :")))
+b = (int(input("Enter how many  numbers :")))
 
-# for i in range(0, b):
-#     a.append(int(input("Enter next number :")))
+for i in range(0, b):
+    a.append(int(input("Enter next number :")))
 
-# for x in a:
-#     print(x,end=" ")
+for x in a:
+    print(x,end=" ")
 
-# from array import *
-# arr = array('i',[1,2,34,23,45,43,67,32])
+from array import *
+arr = array('i',[1,2,34,23,45,43,67,32])
 
-# a = arr.index(43)
-# print(a)
+a = arr.index(43)
+print(a)
 
 # Take both the element and index element to print the elements and the index number from user
 from array import *
