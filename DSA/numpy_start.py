d5d70e8886c68a18edf9db51c4337585a9e41fc9
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-07 19:28:04 +0530

    linspace numpy function

--- DSA/numpy_start.py
+++ DSA/numpy_start.py
@@ -1,4 +1,11 @@
+# from numpy import *
+# a = array([1,2,4,5,6.5,'a'])# numpy is a heterogenius arry so it can acces any typecode
+# for x in a:
+#     print(x,end=" ")
+
+# LINSPACE function
 from numpy import *
-a = array([1,2,4,5,6.5,'a'])# numpy is a heterogenius arry so it can acces any typecode
-for x in a:
-    print(x,end=" ")
+a = linspace(10,20,4) # start,stop,how many number you want
+
+for i in a:
+    print(i,end=" ")
