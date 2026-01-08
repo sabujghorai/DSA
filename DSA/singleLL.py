9c5ec8873b2dfaf2e4ce77bc306b4b8c64988179
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-08 18:49:56 +0530

    printing 10 -> 20 -> 30 -> 40 -> None

--- DSA/singleLL.py
+++ DSA/singleLL.py
@@ -1,5 +1,41 @@
-# Singly Linked List – Basic Structure
+# Node class
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+# Singly Linked List class
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+
+# Driver code
+sll = SinglyLinkedList()
+sll.insert_at_end(10)
+sll.insert_at_end(20)
+sll.insert_at_end(30)
+sll.insert_at_end(40)
+
+sll.print_list()
