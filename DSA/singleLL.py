330d3141964738909a7868436e362a57350d0cce
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-11 18:52:07 +0530

    basics of linked list

--- DSA/singleLL.py
+++ DSA/singleLL.py
@@ -1,31 +1,22 @@
 class Node:
-    def __init__(self,info,next=None):
-        self.info = info
+    def __init__(self,data,next = None):
+        self.data = data
         self.next = next
 
-class SinglyLinkedList:
-    def __init__(self,head=None):
-        self.head = head
+# creating a node
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
 
-    def InsertAtTheEnd(self,value):
-        temp = Node(value)
-        if(self.head != None):
-            t1 = self.head
-            while(t1.next != None):
-                t1 = t1.next 
-            t1.next = temp
-        else:
-            self.head = temp
+# connecting the nodes
+node1.next = node2
+node2.next = node3
+node3.next = node4
 
-    def printLL(self):
-        t1 = self.head
-        while(t1.next != None):
-            print(t1.info)
-            t1 = t1.next
-        print(t1.info)
-
-obj = SinglyLinkedList()
-obj.InsertAtTheEnd(10)
-obj.InsertAtTheEnd(20)
-obj.InsertAtTheEnd(30)
-obj.printLL()
+# printing the nodes
+n1 = node1
+while(n1 != None):
+    print(n1.data,end = "-> ")
+    n1 = n1.next
+print("None")
