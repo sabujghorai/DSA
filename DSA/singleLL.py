99ad47c37f321cb16335e9a2b70996d543ba5bb3
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-12 12:09:02 +0530

    building a new concept

--- DSA/singleLL.py
+++ DSA/singleLL.py
@@ -1,25 +1,25 @@
-class Node:
-    def __init__(self,data,next = None):
-        self.data = data
-        self.next = next
+# class Node:
+#     def __init__(self,data,next = None):
+#         self.data = data
+#         self.next = next
 
-# creating a node
-node1 = Node(10)
-node2 = Node(20)
-node3 = Node(30)
-node4 = Node(40)
+# # creating a node
+# node1 = Node(10)
+# node2 = Node(20)
+# node3 = Node(30)
+# node4 = Node(40)
 
-# connecting the nodes
-node1.next = node2
-node2.next = node3
-node3.next = node4
+# # connecting the nodes
+# node1.next = node2
+# node2.next = node3
+# node3.next = node4
 
-# printing the nodes
-n1 = node1
-while(n1 != None):
-    print(n1.data,end = "-> ")
-    n1 = n1.next
-print("None")
+# # printing the nodes
+# n1 = node1
+# while(n1 != None):
+#     print(n1.data,end = "-> ")
+#     n1 = n1.next
+# print("None")
 
 
 
@@ -29,37 +29,31 @@ class Node:
         self.next = None
 
 
-class SinglleLinkedList:
+class SingleLL:
     def __init__(self):
         self.head = None
 
     def insertAtTheEnd(self, value):
-        new_node = Node(value)
+        temp = Node(value)
 
-        # If list is empty
         if self.head is None:
-            self.head = new_node
-            return
+            self.head = temp
+        else:
+            t1 = self.head
+            while t1.next is not None:
+                t1 = t1.next
+            t1.next = temp   # linking the new node
 
-        # Traverse till last node
-        temp = self.head
-        while temp.next is not None:
-            temp = temp.next
+    def printLL(self):
+        t1 = self.head
+        while t1 is not None:
+            print(t1.data)
+            t1 = t1.next
 
-        # Insert at end
-        temp.next = new_node
 
-    def display(self):
-        temp = self.head
-        while temp:
-            print(temp.data, end=" -> ")
-            temp = temp.next
-        print("None")
-
-objects = SinglleLinkedList()
-objects.insertAtTheEnd(100)
-objects.insertAtTheEnd(200)
-objects.insertAtTheEnd(300)
-objects.insertAtTheEnd(400)
-
-objects.display()
+obj = SingleLL()
+obj.insertAtTheEnd(10)
+obj.insertAtTheEnd(20)
+obj.insertAtTheEnd(30)
+obj.insertAtTheEnd(40)
+obj.printLL()
