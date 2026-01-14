b9163c4a1070eeebdb655f725a87b7c68a55070a
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-14 18:02:08 +0530

    uncommenting all

--- DSA/singleLL.py
+++ DSA/singleLL.py
@@ -1,63 +1,63 @@
-# class Node:
-#     def __init__(self,data,next = None):
-#         self.data = data
-#         self.next = next
+class Node:
+    def __init__(self,data,next = None):
+        self.data = data
+        self.next = next
 
-# # creating a node
-# node1 = Node(10)
-# node2 = Node(20)
-# node3 = Node(30)
-# node4 = Node(40)
+# creating a node
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
 
-# # connecting the nodes
-# node1.next = node2
-# node2.next = node3
-# node3.next = node4
+# connecting the nodes
+node1.next = node2
+node2.next = node3
+node3.next = node4
 
-# # printing the nodes
-# n1 = node1
-# while(n1 != None):
-#     print(n1.data,end = "-> ")
-#     n1 = n1.next
-# print("None")
-
-
-
-# # New linked list
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.next = None
+# printing the nodes
+n1 = node1
+while(n1 != None):
+    print(n1.data,end = "-> ")
+    n1 = n1.next
+print("None")
 
 
-# class SingleLL:
-#     def __init__(self):
-#         self.head = None
 
-#     def insertAtTheEnd(self, value):
-#         temp = Node(value)
-
-#         if self.head is None:
-#             self.head = temp
-#         else:
-#             t1 = self.head
-#             while t1.next is not None:
-#                 t1 = t1.next
-#             t1.next = temp   # linking the new node
-
-#     def printLL(self):
-#         t1 = self.head
-#         while t1 is not None:
-#             print(t1.data)
-#             t1 = t1.next
+# New linked list
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
-# obj = SingleLL()
-# obj.insertAtTheEnd(10)
-# obj.insertAtTheEnd(20)
-# obj.insertAtTheEnd(30)
-# obj.insertAtTheEnd(40)
-# obj.printLL()
+class SingleLL:
+    def __init__(self):
+        self.head = None
+
+    def insertAtTheEnd(self, value):
+        temp = Node(value)
+
+        if self.head is None:
+            self.head = temp
+        else:
+            t1 = self.head
+            while t1.next is not None:
+                t1 = t1.next
+            t1.next = temp   # linking the new node
+
+    def printLL(self):
+        t1 = self.head
+        while t1 is not None:
+            print(t1.data)
+            t1 = t1.next
+
+
+obj = SingleLL()
+obj.insertAtTheEnd(10)
+obj.insertAtTheEnd(20)
+obj.insertAtTheEnd(30)
+obj.insertAtTheEnd(40)
+obj.printLL()
 
 
 class Node:
