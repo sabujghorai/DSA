6491052ac84c6a4adcdebc9256ed8646f6c78abc
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-15 11:16:55 +0530

    insert at the middle

--- DSA/singleLL.py
+++ DSA/singleLL.py
@@ -60,7 +60,49 @@
 # obj.printLL()
 
 
-# Insert at the begeining
+# # Insert at the begeining
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+
+# class SingleLL:
+#     def __init__(self):
+#         self.head = None
+
+#     def insertAtTheEnd(self, value):
+#         temp = Node(value)
+#         if self.head is None:
+#             self.head = temp
+#         else:
+#             t1 = self.head
+#             while t1.next is not None:
+#                 t1 = t1.next
+#             t1.next = temp   # linking the new node
+
+#     def insertAtBeg(self,value):
+#         temp = Node(value) 
+#         temp.next = self.head # prints the value --> 5
+#         self.head = temp # prints the other values
+
+#     def printLL(self):
+#         t1 = self.head
+#         while t1 is not None:
+#             print(t1.data)
+#             t1 = t1.next
+
+
+# obj = SingleLL()
+# obj.insertAtTheEnd(10)
+# obj.insertAtTheEnd(20)
+# obj.insertAtTheEnd(30)
+# obj.insertAtTheEnd(40)
+# obj.insertAtBeg(5)
+# obj.printLL()
+
+
+# intert at the middle
 class Node:
     def __init__(self, data):
         self.data = data
@@ -81,10 +123,14 @@ class SingleLL:
                 t1 = t1.next
             t1.next = temp   # linking the new node
 
-    def insertAtBeg(self,value):
-        temp = Node(value) 
-        temp.next = self.head # prints the value --> 5
-        self.head = temp # prints the other values
+    def insertAtMid(self,value,x):
+        temp = Node(value)
+        t1 = self.head
+        while(t1.next != None):
+            if(t1.data == x):
+                temp.next = t1.next
+                t1.next = temp
+            t1 = t1.next
 
     def printLL(self):
         t1 = self.head
@@ -98,5 +144,5 @@ obj.insertAtTheEnd(10)
 obj.insertAtTheEnd(20)
 obj.insertAtTheEnd(30)
 obj.insertAtTheEnd(40)
-obj.insertAtBeg(5)
+obj.insertAtMid(40,20)
 obj.printLL()
