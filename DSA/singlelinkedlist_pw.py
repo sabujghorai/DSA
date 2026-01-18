053433d6511e0965aa6c81dddb841b256d68191a
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-18 11:45:09 +0530

    adding some value

--- DSA/singlelinkedlist_pw.py
+++ DSA/singlelinkedlist_pw.py
@@ -5,7 +5,17 @@ class Node:
 a = Node(5)
 b = Node(3)
 c = Node(8)
+d = Node(12)
+e = Node(13)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
 
 head = a
 print(head.data)
-# print(head.next.data)
+print(head.next.data)
+print(head.next.next.data)
+print(head.next.next.next.data)
+print(head.next.next.next.next)
