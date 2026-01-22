7a66d1c363a206a95ca1f81b602a8948be8462c6
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-22 22:42:03 +0530

    adding a new node

--- DSA/singlelinkedlist_pw.py
+++ DSA/singlelinkedlist_pw.py
@@ -1,70 +1,115 @@
+# class Node:
+#     def __init__(self,data):
+#         self.data = data
+#         self.next = None
+# a = Node(5)
+# b = Node(6)
+# c = Node(8)
+# d = Node(12)
+# e = Node(13)
+
+# a.next = b
+# b.next = c
+# c.next = d
+# d.next = e
+
+# head = a
+
+# # Traverse linked list
+# def printLL(head): #writing as a function
+#     curr = head
+#     while curr != None :
+#         print(curr.data)
+#         curr = curr.next # moves to next node
+
+# # insert at the end
+# NewNode1 = Node(14)
+# curr = head
+# while curr.next != None:
+#     curr = curr.next
+# curr.next = NewNode1
+
+
+
+# #insert at the begeining
+# NewNode = Node(4)
+# NewNode.next = head
+# head = NewNode
+
+# # insertion at the middle at the k th index
+# k = 2
+# NewNode = Node(7)
+
+# curr = head
+# for i in range(k-1):
+#     curr = curr.next
+
+# NewNode.next = curr.next
+# curr.next = NewNode
+
+
+
+
+# # delete the first node
+# head = head.next
+
+# # deleting the last Index
+# curr = head
+# while curr.next.next != None:
+#     curr = curr.next
+
+# curr.next = None
+
+# # deleting the k-th index
+# k = 2
+# curr = head
+# for i in range(k-1):
+#     curr = curr.next
+# curr.next = curr.next.next # delete the 2nd index = 8
+
+# printLL(head)
+
+
+
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
-a = Node(5)
-b = Node(6)
-c = Node(8)
-d = Node(12)
-e = Node(13)
 
-a.next = b
-b.next = c
-c.next = d
-d.next = e
+class SinglleLL:
+    def middleNode(self, head):
+        curr = head
+        l = 0
 
-head = a
+        # count length
+        while curr is not None:
+            l += 1
+            curr = curr.next
 
-# Traverse linked list
-def printLL(head): #writing as a function
-    curr = head
-    while curr != None :
-        print(curr.data)
-        curr = curr.next # moves to next node
+        # reset curr to head
+        curr = head
 
-# insert at the end
-NewNode1 = Node(14)
-curr = head
-while curr.next != None:
-    curr = curr.next
-curr.next = NewNode1
+        # move to middle
+        for _ in range(l // 2):
+            curr = curr.next
+
+        return curr
 
 
+# create nodes
+n1 = Node(10)
+n2 = Node(20)
+n3 = Node(30)
+n4 = Node(40)
+n5 = Node(50)
 
-#insert at the begeining
-NewNode = Node(4)
-NewNode.next = head
-head = NewNode
+# linking
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
 
-# insertion at the middle at the k th index
-k = 2
-NewNode = Node(7)
-
-curr = head
-for i in range(k-1):
-    curr = curr.next
-
-NewNode.next = curr.next
-curr.next = NewNode
-
-
-
-
-# delete the first node
-head = head.next
-
-# deleting the last Index
-curr = head
-while curr.next.next != None:
-    curr = curr.next
-
-curr.next = None
-
-# deleting the k-th index
-k = 2
-curr = head
-for i in range(k-1):
-    curr = curr.next
-curr.next = curr.next.next # delete the 2nd index = 8
-
-printLL(head)
+# call method
+ll = SinglleLL()
+mid = ll.middleNode(n1)
+print("Middle Node:", mid.data)
